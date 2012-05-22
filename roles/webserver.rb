e0adbaebd4ef18c9279924760420ae2bbbf3953c
Mike Fiedler <miketheman@gmail.com>
2012-05-21 21:50:19 -0400

    adding a slightly modified version of the apache2 cookbook - this adds the abiltiy to set the prefrok binary mode via attribute

--- roles/webserver.rb
+++ roles/webserver.rb
@@ -3,6 +3,7 @@ description "bottle.py webapp for fullstack"
 run_list("recipe[fullstack::webapp]")
 override_attributes(
   "apache" => {
-    "listen_ports" => [ "8080", "443" ]
+    "listen_ports" => [ "8080", "443" ],
+    "mpm_binary" => "httpd.worker"
   }
 )
