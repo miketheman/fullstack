8010a31970a554025bf23a33e4fc3582ee6a6829
Mike Fiedler <miketheman@gmail.com>
2013-04-20 16:19:59 -0400

    enable admin on haproxy for datadog monitoring

--- roles/load_balancer.rb
+++ roles/load_balancer.rb
@@ -6,6 +6,9 @@ run_list %w{
 }
 override_attributes(
   'haproxy' => {
+    'admin' => {
+     'address_bind' => "0.0.0.0" # In prodcution, remove this for security
+    },
     'app_server_role' => "webserver",
     'member_port' => "8080"
   },
