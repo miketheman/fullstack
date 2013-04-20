0351c69392e4e1922d9006573009d893dea6c93c
Mike Fiedler <miketheman@gmail.com>
2013-04-20 16:38:13 -0400

    fullstack 0.2.0

--- site-cookbooks/fullstack/metadata.rb
+++ site-cookbooks/fullstack/metadata.rb
@@ -1,9 +1,10 @@
+name             "fullstack"
 maintainer       "Mike Fiedler"
 maintainer_email "miketheman@gmail.com"
 license          "All rights reserved"
 description      "Installs/Configures fullstack"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.2.0"
 
 depends "apache2"
 depends "datadog"
